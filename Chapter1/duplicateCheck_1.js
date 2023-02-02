405fea3a0a0c546b478638570e23004246fae601
dev-taein <dev.taein@gmail.com>
2023-02-02 19:21:32 +0900

    23-02-02 update

--- Chapter1/duplicateCheck_1.js
+++ Chapter1/duplicateCheck_1.js
@@ -1,5 +1,5 @@
-// Page 번호 : 
-// Page 문제 이름 : 
+// Page 번호 : c4.2, p92
+// Page 문제 이름 : 빅오로 코드 속도 올리기 
 
 // 중복 숫자 찾기 O(n)
 // 아래 예제 코드의 O(n)은 메모리를 많이 사용한다. 
@@ -16,6 +16,6 @@ function hasarray(array){
     return false;
 }
 
-hasarray([3,5,8])
+hasarray([3,5,8,5])
 
 
--- Chapter1/duplicateCheck_2.js
+++ Chapter1/duplicateCheck_2.js
@@ -1,10 +1,12 @@
+// Page 번호 : c4.2, p92
+// Page 문제 이름 : 빅오로 코드 속도 올리기 
+
 // 중복 숫자 찾기 O(n2)
 function fasarray(array) {
     let index = [];
     for (let i = 0; i < array.length; i++){
         for(let j = 0; j < array.length; j++){
             if( i !== j && array[i] === array[j]) {
-                console.timeEnd('calculatingTime')
                 return true;
             }
         }
